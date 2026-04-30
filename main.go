89de863d9bdb9f8ce9a95fad84d662ff511c421f
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-04-30 10:29:02 -0600

    make bot react to messages on validity

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
@@ -50,11 +52,44 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	log.Println("message:", m.Message.ID, m.Author.Username, "sent", m.Message.Content)
-	s.MessageReactionAdd(m.ChannelID, m.Message.ID, "✅")
+	log.Println("message:", m.Message.ID, m.Author.Username, "sent:", m.Message.Content)
+	handleCheck(s, m)
+}
 
-	// Respond to a simple command "!ping"
-	if m.Content == "!ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
+type check struct {
+	name      string
+	discordID string
+	messageID string
+	date      uint64
+	checkType string
+}
+
+func handleCheck(s *discordgo.Session, m *discordgo.MessageCreate) {
+	checkType, date, err := parseCheckMessage(m.Message.Content)
+	check := check{
+		name:      m.Author.Username,
+		discordID: m.Author.ID,
+		messageID: m.Message.ID,
+		date:      date,
+		checkType: checkType,
 	}
+	if err != nil {
+		s.MessageReactionAdd(m.ChannelID, m.Message.ID, "❌")
+		log.Println("ERROR: unable to check in", check.name, "in message", check.messageID, err)
+		return
+	}
+
+	s.MessageReactionAdd(m.ChannelID, m.Message.ID, "✅")
+	log.Println("checked in", check.name, "in message", check.messageID)
+}
+
+func parseCheckMessage(message string) (string, uint64, error) {
+	r, _ := regexp.Compile("(in|out)")
+
+	match := r.MatchString(message)
+	if !match {
+		return "", 0, errors.New("invalid command supplied")
+	}
+
+	return "", 0, nil
 }
