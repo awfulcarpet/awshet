9c7537cdf171a9bcf5cd60f08b04988b9fd959df
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-04-30 19:50:14 -0600

    add parsing of hh:mm 24hr dates

--- parsing.go
+++ parsing.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
-func parseCheckMessage(message string) (time.Time, error) {
-	nowRegex := regexp.MustCompile("now")
-
-	date := nowRegex.FindString(message)
-	if date == "" {
-		return time.Time{}, errors.New("invalid time supplied")
+func parseTime(str string) (int, int, error) {
+	hour_and_minute := strings.Split(str, ":")
+	hour, err := strconv.Atoi(hour_and_minute[0])
+	if err != nil {
+		return -1, -1, errors.New("invalid number")
 	}
 
-	time := time.Now()
+	minute, err := strconv.Atoi(hour_and_minute[1])
+	if err != nil {
+		return -1, -1, errors.New("invalid number")
+	}
 
-	return time, nil
+	return hour, minute, nil
+}
+
+func parseCheckMessage(message string) (time.Time, error) {
+	nowRegex := regexp.MustCompile("now")
+	hhmmRegex := regexp.MustCompile("([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]")
+	currentTime := time.Now()
+
+	if nowRegex.FindString(message) == "now" {
+		return time.Now(), nil
+	}
+
+	date := hhmmRegex.FindString(message)
+	if date != "" {
+		hour, minute, err := parseTime(date)
+		if err != nil {
+			return time.Time{}, fmt.Errorf("invalid time supplied: %s", err)
+		}
+		return time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), hour, minute, 0, 0, time.UTC), nil
+	}
+
+	return time.Time{}, errors.New("invalid time supplied: unsupported format")
 }
